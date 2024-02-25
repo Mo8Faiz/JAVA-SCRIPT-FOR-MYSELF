e3e975072efa07400978e6c41fe90f44e6e626a7
Md Faiz <149877763+Mo8Faiz@users.noreply.github.com>
2024-02-25 11:01:05 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+"use strict";
+//number (int)
+//number(7);
+let number,num1 =30
+//boolean (true/false)
+//Boolean(false);
+let Boolean, f=false,t=true
+//string 
+//String("Forever21");
+let String,str="Forever Knight"
+//undifined
+let num2 //num1 here is undifined
+//null
+console.table([typeof num1,typeof f,typeof t,typeof str,typeof num2,typeof null])
+
